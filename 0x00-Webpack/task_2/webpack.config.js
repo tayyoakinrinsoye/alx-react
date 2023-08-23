c69e05a5f31300cb45b1ed13e386f7e4b450f872
Tayo Akinrinsoye <tayo.akinrinsoye@hotmail.com>
2023-08-23 14:09:54 +0100

    Another update

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -12,23 +12,14 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+      { 
+        test: /\.css$/, 
+        use: ["style-loader", "css-loader"] 
       },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
-            },
-          },
-        ],
+      { 
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
       },
-    ],
-  },
+    ]
+    },
 };
